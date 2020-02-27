e4c28289318962be848cee94974b8d2abe9716a4
tseaver6 <dancerfool@gmail.com>
2020-02-27 16:53:20 -0500

    Fixing up and regex stuff

--- src/main/webapp/js/signupScript.js
+++ src/main/webapp/js/signupScript.js
@@ -1,14 +1,17 @@
 $(document).ready(function() {
-	$("button").click(function() {
-		var email = $("#email").val();
-		var pwd = $("#password").val();
+    $("button").click(function() {
         var emailRegex = /^A-Za-z0-9@(?:[0-9a-zA-Z-]+.)+[a-zA-Z]{2,9}$/
         var passwordRegex = /^(?=.\d)(?=.[a-z])(?=.[A-Z])(?=.[a-zA-Z]).{8,}$/
+
+        var email = $("#email").val();
+        var pwd = $("#password").val();
+        var fName = $("#firstName").val();
+        var lName = $("#lastName").val();
         if (emailRegex.test(email) && passwordRegex.test(pwd)) {
             $.ajax({
                 type: 'POST',
                 url: '/hello/postItLocalSearch',
-                data: {'Email': email,'Password':pwd},
+                data: {'Email': email,'Password':pwd, 'First Name': fName, 'Last Name': lName},
                 success: function(dataFromServer) {
                     var result = JSON.parse(dataFromServer);
                     console.log("Server has succeeded.");
@@ -20,6 +23,5 @@ $(document).ready(function() {
         } else {
 
         }
-
     });
 });
